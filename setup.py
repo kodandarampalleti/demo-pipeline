fc2cf238a7bba33c9f3b9002789c10d7dd181aa7
kodandarampalleti <palletikodandaram@gmail.com>
2023-03-28 12:40:58 +0530

    all file add

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name = "census-income",
+version = "0.0.1",author_email = "alwayssram@gmail.com",
+packages = find_packages() , install_requires = ["pandas" , "numpy" , "flask"])
