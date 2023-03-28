f93f82e45449c289c0106eb89ffe181c76b3076d
kodandarampalleti <palletikodandaram@gmail.com>
2023-03-28 12:50:11 +0530

    all file add

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
 
-setup(name = "census-income",
-version = "0.0.1",author_email = "alwayssram@gmail.com",
+setup(name = "census-income", version = "0.0.1",author_email = "alwayssram@gmail.com",
 packages = find_packages() , install_requires = ["pandas" , "numpy" , "flask"])
